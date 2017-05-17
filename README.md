4a366c76414b4014a2410b9bc4a6767739e88707
cag2050 <cag2050@163.com>
2017-05-17 17:30:53 +0800

    m

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
+* 运行 Application.java 方式来启动Spring Boot项目，下面3种写法中选择一种  
+写法1，最简单的写法：  
+```  
+SpringApplication.run(Application.class, args);  
+```  
+该方法返回一个ApplicationContext对象，使用注解的时候返回的具体类型是AnnotationConfigApplicationContext或AnnotationConfigEmbeddedWebApplicationContext，当支持web的时候是第二个。  
+写法2：  
+```  
+SpringApplication application = new SpringApplication(Application.class);  
+application.run(args);  
+```  
+SpringApplication包含了一些其他可以配置的方法，如果你想做一些配置，可以用这种方式。  
+写法3：使用SpringApplicationBuilder  
+```  
+new SpringApplicationBuilder()
+        .showBanner(false)
+        .sources(Application.class)
+        .run(args);  
+```  
+当使用SpringMVC的时候由于需要使用子容器，就需要用到SpringApplicationBuilder，该类有一个child(xxx...)方法可以添加子容器。
+
+
+
 参考链接：http://blog.csdn.net/isea533/article/details/50278205
--- src/main/java/com/cag/spring_boot_demo/Application.java
+++ src/main/java/com/cag/spring_boot_demo/Application.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,13 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		// 运行 Application.java 来启动Spring Boot项目，下面3种写法中选择一种
+		// 写法1：
+//		SpringApplication.run(Application.class, args);
+		// 写法2：
+		SpringApplication application = new SpringApplication(Application.class);
+		application.run(args);
+		// 写法3：
+//		new SpringApplicationBuilder().showBanner(false).sources(Application.class).run(args);
 	}
-
 }
